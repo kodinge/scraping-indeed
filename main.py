fcafcb21eb607b598087e75a126bdbd4a7ab4a26
tedymaradho <maradho@gmail.com>
2021-12-04 21:31:45 +0700

    save all data from all pages

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ import os
 import pandas as pd
 
 url = 'https://id.indeed.com/jobs?'
-# params = {
-#     'q': 'Python Development',
-#     'l': 'Jakarta'
-# }
-# headers = {
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-#                   'Chrome/96.0.4664.45 Safari/537.36'
-# }
-# res = requests.get(url, params=params, headers=headers)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/96.0.4664.45 Safari/537.36'
+}
+
 indeeds = None
 
 
@@ -21,11 +17,6 @@ def get_total_pages(query, location):
     params = {
         'q': query,
         'l': location
-    }
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/96.0.4664.45 Safari/537.36'
     }
 
     res = requests.get(url, params=params, headers=headers)
@@ -55,11 +46,6 @@ def get_all_items(query, location, start, page):
         'q': query,
         'l': location,
         'start': start
-    }
-
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/96.0.4664.45 Safari/537.36'
     }
 
     res = requests.get(url, params=params, headers=headers)
@@ -107,7 +93,6 @@ def create_document(dataFrame, filename):
     except FileExistsError:
         pass
 
-
     df = pd.DataFrame(dataFrame)
     df.to_csv(f'data_result/{filename}.csv', index=False)
     df.to_excel(f'data_result/{filename}.xlsx', index=False)
@@ -138,6 +123,7 @@ def run():
     print('Data Json Created')
 
     create_document(final_result, query)
+
 
 if __name__ == '__main__':
     run()
